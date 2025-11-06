865c1f16a2f1f75f74578b2c14588eec3ba50625
misaka0714 <129934985+misaka0714@users.noreply.github.com>
2025-11-06 09:41:11 +0800

    Add SampleTrainingData function declaration
    Signed-off-by: misaka0714 <129934985+misaka0714@users.noreply.github.com>

--- src/utils/util_functions.h
+++ src/utils/util_functions.h
@@ -130,4 +130,11 @@ set_dataset(DataTypes type,
             const void* vectors_ptr,
             uint32_t num_element);
 
+DatasetPtr
+SampleTrainingData(const DatasetPtr& data,
+                   int64_t total_elements,
+                   int64_t dim,
+                   int64_t train_sample_count,
+                   Allocator* allocator = nullptr);
+
 }  // namespace vsag
